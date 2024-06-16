e52e3562b50c0bf60f10c4bddc9ebbdfb81d75f0
Espiobest <59823894+Espiobest@users.noreply.github.com>
2024-06-16 18:21:54 +0530

    add slider images dynamically

--- js/carousel-script.js
+++ js/carousel-script.js
@@ -1,4 +1,23 @@
 $(document).ready(function(){
+    // dynamically add images to the carousel
+    const numberOfImages = 11;
+    for (let i = 0; i < numberOfImages; i++) {
+        // Create the div element
+        let sliderCardDiv = $('<div>').addClass('slider-card');
+        // Create the inner div
+        let innerDiv = $('<div>').addClass('d-flex justify-content-center align-items-center mb-4');
+        // Create the image element
+        let imgElement = $('<img>').attr('src', 'assets/img/our_model_carousel/img' + i + '.png')
+                                    .attr('alt', '#')
+                                    .css({'width': '100rem', 'height': '250px'});
+        // Append image to inner div
+        innerDiv.append(imgElement);
+        // Append inner div to slider card div
+        sliderCardDiv.append(innerDiv);
+        // Append slider card div to owl-carousel
+        $('.owl-carousel').append(sliderCardDiv);
+    }
+
     $('.owl-carousel').owlCarousel({
         loop:true,
         margin:10,
@@ -37,23 +56,3 @@ $('.owl-carousel').on('click', '.owl-dots, .owl-nav', function(e) {
 	$(this).closest('.owl-carousel').trigger('stop.owl.autoplay');
 	$(this).closest('.owl-carousel').trigger('play.owl.autoplay');
 });
-
-// $(document).ready(function() {
-//     // Loop through each image div
-//     for (let i = 0; i < 10; i++) {
-//         // Create the div element
-//         let sliderCardDiv = $('<div>').addClass('slider-card');
-//         // Create the inner div
-//         let innerDiv = $('<div>').addClass('d-flex justify-content-center align-items-center mb-4');
-//         // Create the image element
-//         let imgElement = $('<img>').attr('src', 'assets/img/our_model_carousel/img' + i + '.png')
-//                                     .attr('alt', '#')
-//                                     .css({'width': '100rem', 'height': '250px'});
-//         // Append image to inner div
-//         innerDiv.append(imgElement);
-//         // Append inner div to slider card div
-//         sliderCardDiv.append(innerDiv);
-//         // Append slider card div to owl-carousel
-//         $('.owl-carousel').append(sliderCardDiv);
-//     }
-// });
