05a012b2a5268045e4d6b61f0f8470ab96e7f412
Shreya V <vallachirashreya@gmail.com>
2025-01-25 16:31:22 -0500

    Update news-cards.js
    Changes to the first three posts based on articles

--- js/news-cards.js
+++ js/news-cards.js
@@ -1,24 +1,21 @@
 const cardData = [
     {
-        imgSrc: 'nc-carousel-images/img2.png',
-        heading: '300 Members Milestone',
-        about: 'Instilt Educate is thrilled to announce that we have reached 300 members! Starting as a high schooler-run nonprofit, we have grown globally with students from India to Rwanda. Our dedicated tutors ensure a rewarding experience for all.',
-        link: 'https://www.linkedin.com/posts/instilt-edu_instilt-instilteducate-nonprofit-activity-7169154605942038528-QTed?utm_source=share&utm_medium=member_desktop',
-        isLinkedInPost: true
+        imgSrc: 'nc-carousel-images/RWEN19-grad1.png',
+        heading: 'RWEN19s Graduation',
+        about: 'In 2023, we launched our first cohort in Rwanda, marking our inaugural venture into a new continent. Partnering with the Rwanda Girls Initiative, we empowered bright female students through immersive English learning and educational support, demonstrating the impact of our program.',
+        link: 'https://docs.google.com/document/d/1yegh-gNAYybsg7TZFSNV9rXnRvHnT-XIM2WH7bdMu2A/edit?tab=t.0',
     },  
     {
-        imgSrc: 'nc-carousel-images/img6.png',
-        heading: 'Meet Batch INEN23',
-        about: 'Meet Batch INEN23!!🧡👩🏻‍🤝‍👨🏽 thank you to our fantastic batch managers and tutors for their commitment, support and always making class a fun time🫶',
-        link: 'https://www.instagram.com/p/CyZ5O8IOTOM/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==',
-        isInstagramPost: true
+        imgSrc: 'nc-carousel-images/emp-edu.png',
+        heading: 'Empowering Education',
+        about: 'This year, we expanded education initiatives in Africa, India, and beyond. Partnering with Barefoot College International, we launched English programs in Senegal, Madagascar, Zanzibar, and Guatemala. In Punjab, India, we enhanced English proficiency for students and teachers to boost access to higher education.',
+        link: 'https://docs.google.com/document/d/1yegh-gNAYybsg7TZFSNV9rXnRvHnT-XIM2WH7bdMu2A/edit?tab=t.0',
     },
     {
-        imgSrc: 'nc-carousel-images/img3.png',
-        heading: 'Youthify Collaboration',
-        about: 'We are excited to announce an exciting collaboration with Youthify! Youthify is a non-profit based in India that aims to dismantle the stigma around mental health and spread awareness. We hope this collaboration shines a light on an important issue: the importance of mental health in education.',
-        link: 'https://www.linkedin.com/posts/instilt-edu_instilt-educate-is-excited-to-announce-an-activity-7249524576374681602-6bpJ?utm_source=share&utm_medium=member_desktop',
-        isLinkedInPost: true
+        imgSrc: 'nc-carousel-images/INEN28grad.png',
+        heading: 'INEN28s Graduation',
+        about: 'In July, our first batch with Salaam Baalak Trust saw 14 high school graduates transform through Instilts lessons.They improved their English and experienced personal growth, celebrated in a heartfelt graduation ceremony honoring their journey and hard work.',
+        link: 'https://docs.google.com/document/d/1yegh-gNAYybsg7TZFSNV9rXnRvHnT-XIM2WH7bdMu2A/edit?tab=t.0',
     }, 
     {
         imgSrc: 'nc-carousel-images/img7.png',
@@ -95,6 +92,25 @@ function createPosts(cards) {
                     <img src="${card.imgSrc}" style="height: 250px; border-radius: 5px">
                     <i class="fa-brands fa-linkedin news-appIcon"></i>
                         <h3 class="article-heading" id="interactive" style="font-size: 20px">${card.heading}</h3>
+                    
+                    <div class="news-text-container">
+                        <p class="article-about small" id="interactive-about">${card.about}</p>
+                    </div>
+                    <a href="${card.link}">
+                        <div class="button-container">
+                            <button class="news-button1"> Read More </button>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        `;
+        }
+        else {
+            cardElement.innerHTML = `
+            <div class="news-card1">
+                <div class="news-card-body text-center">
+                    <img src="${card.imgSrc}" style="height: 250px; border-radius: 5px">
+                    <h3 class="article-heading" id="interactive" style="font-size: 20px">${card.heading}</h3>
                     
                     <div class="news-text-container">
                         <p class="article-about small" id="interactive-about">${card.about}</p>
