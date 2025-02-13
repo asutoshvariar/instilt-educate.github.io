7fe6efb8d539c515e606dc9bf90aa630f7487f82
Espiobest <59823894+Espiobest@users.noreply.github.com>
2025-02-12 23:31:25 -0500

    make card styling consistent

--- js/news-cards.js
+++ js/news-cards.js
@@ -14,7 +14,7 @@ const cardData = [
     {
         imgSrc: '../assets/img/news-cards/INEN28grad.png',
         heading: 'Salaam Baalak Trust - First Batch Graduation!',
-        about: 'In July, our first batch with Salaam Baalak Trust saw 14 high school graduates transform through Instilts lessons.They improved their English and experienced personal growth, celebrated in a heartfelt graduation ceremony honoring their journey and hard work.',
+        about: 'In July, our first batch with Salaam Baalak Trust saw 14 high school graduates transform through Instilt\'s lessons. They improved their English and experienced personal growth, celebrated in a heartfelt graduation ceremony honoring their journey and hard work.',
         link: 'https://instilts-newsletter-0a931b.beehiiv.com/p/salaam-baalak-trust-batch-s-graduation',
     }, 
 ];
@@ -27,85 +27,32 @@ function createPosts(cards) {
         const cardElement = document.createElement('div');
         cardElement.classList.add('news-col-md-4');
 
+        let iconClass = "fa-brands fa-linkedin";
+        if (card.type?.toLowerCase() === "instagram") {
+            iconClass = "fa-brands fa-instagram";
+        } else if (card.type?.toLowerCase() === "linkedin") {
+            iconClass = "fa-brands fa-linkedin";
+        }
         cardElement.innerHTML = `
-            <div class="news-card1">
-                <div class="news-card-body text-center">
-                    <img src="${card.imgSrc}" style="height: 250px border-radius: 5px">
-                        <h3 class="news-heading-container" id="interactive">${card.heading}</h3>
-                    <div class="news-text-container">
-                        <p class="article-about small" id="interactive-about" margin-top: 1 flex-grow: 1>${card.about}</p>
-                    </div>
+        <div class="news-card1">
+            <div class="news-card-body text-center">
+                <img src="${card.imgSrc}" class="news-card-img">
+                <h3 class="news-heading-container" id="interactive">${card.heading}</h3>
+                
+                <div class="news-text-container">
+                    <p class="article-about medium" id="interactive-about">${card.about}</p>
+                </div>
+                
+                <div class="button-container">
                     <a href="${card.link}" target="_blank">
-                        <div class="button-container">
-                            <button class="news-button1" flex-grow: 1> Read More </button>
-                        </div>
+                        <button class="news-button1"> Read More </button>
                     </a>
                 </div>
             </div>
+        </div>
         `;
 
-        if(card.isInstagramPost) {
-            cardElement.innerHTML = `
-            <div class="news-card1">
-                <div class="news-card-body text-center">
-                    <img src="${card.imgSrc}" style="height: 250px; border-radius: 5px">
-                    <i class="fa-brands fa-instagram news-appIcon"></i>
-                        <h3 class="news-heading-container" id="interactive">${card.heading}</h3>
-                    
-                    <div class="news-text-container">
-                        <p class="article-about small" id="interactive-about">${card.about}</p>
-                    </div>
-                    <a href="${card.link}">
-                        <div class="button-container">
-                            <button class="news-button1"> Read More </button>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        `;
-        }
-        else if(card.isLinkedInPost){
-            cardElement.innerHTML = `
-            <div class="news-card1">
-                <div class="news-card-body text-center">
-                    <img src="${card.imgSrc}" style="height: 250px; border-radius: 5px">
-                    <i class="fa-brands fa-linkedin news-appIcon"></i>
-                    <h3 class="news-heading-container" id="interactive">${card.heading}</h3>
-                    
-                    <div class="news-text-container">
-                        <p class="article-about small" id="interactive-about">${card.about}</p>
-                    </div>
-                    <a href="${card.link}">
-                        <div class="button-container">
-                            <button class="news-button1"> Read More </button>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        `;
-        }
-        else {
-            cardElement.innerHTML = `
-            <div class="news-card1">
-                <div class="news-card-body text-center">
-                    <img src="${card.imgSrc}" style="height: 250px; border-radius: 5px">
-                    <h3 class="news-heading-container" id="interactive" style="font-size: 19px">${card.heading}</h3>
-                    
-                    <div class="news-text-container">
-                        <p class="article-about small" id="interactive-about">${card.about}</p>
-                    </div>
-                    
-                    <a href="${card.link}">
-                        <div class="button-container">
-                            <button class="news-button1"> Read More </button>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        `;
-        }
-
-    container.appendChild(cardElement);
+        container.appendChild(cardElement);
     });
 }
 
