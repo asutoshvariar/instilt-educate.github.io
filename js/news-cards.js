0e850b61ced4d8227e9619c9f8f36c3c6db598a6
Espiobest <59823894+Espiobest@users.noreply.github.com>
2025-02-16 13:06:01 -0500

    update news pics

--- js/news-cards.js
+++ js/news-cards.js
@@ -1,18 +1,18 @@
 const cardData = [
     {
-        imgSrc: '../assets/img/news-cards/RWEN19-spotlight.png',
+        imgSrc: '../assets/img/news-cards/RwandaGrad.png',
         heading: 'Celebrating the Completion of our First Africa Batch!',
         about: 'In 2023, we launched our first cohort in Rwanda, marking our inaugural venture into a new continent. Partnering with the Rwanda Girls Initiative, we empowered bright female students through immersive English learning and educational support, demonstrating the impact of our program.',
         link: 'https://instilts-newsletter-0a931b.beehiiv.com/p/celebrating-the-completion-of-our-first-africa-batch',
     },  
     {
-        imgSrc: '../assets/img/news-cards/BCI-colab.png',
+        imgSrc: '../assets/img/news-cards/emp-edu.png',
         heading: 'Empowering Education: New Batches in India and Africa',
         about: 'This year, we expanded education initiatives in Africa, India, and beyond. Partnering with Barefoot College International, we launched English programs in Senegal, Madagascar, Zanzibar, and Guatemala. In Punjab, India, we enhanced English proficiency for students and teachers to boost access to higher education.',
         link: 'https://instilts-newsletter-0a931b.beehiiv.com/p/empowering-education-new-batches-in-india-and-africa-and-launching-the-india-council',
     },
     {
-        imgSrc: '../assets/img/news-cards/graduation.jpg',
+        imgSrc: '../assets/img/news-cards/INEN28grad.png',
         heading: 'Salaam Baalak Trust - First Batch Graduation!',
         about: 'In July, our first batch with Salaam Baalak Trust saw 14 high school graduates transform through Instilt\'s lessons. They improved their English and experienced personal growth, celebrated in a heartfelt graduation ceremony honoring their journey and hard work.',
         link: 'https://instilts-newsletter-0a931b.beehiiv.com/p/salaam-baalak-trust-batch-s-graduation',
